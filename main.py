b26a92d1e1480a73de4ce5ebe6ea4630fb3bfbc8
Fred Sauer <fredsa@google.com>
2014-03-04 13:31:04 -0800

    Add custom 500 error handler so app handler errors aren't supressed

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ def hello():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
