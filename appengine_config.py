e83cb97d0ec6ca3facbd72dcddd8397243704cb5
Michael Bernstein <michael@fandomhome.com>
2014-10-04 18:03:44 -0500

    Made consistent with final version of vendor.py code.

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-import site
-import os.path
-# add `lib` subdirectory as a site packages directory, so our `main` module can load
-# third-party libraries.
-site.addsitedir(os.path.join(os.path.dirname(__file__), 'lib'))
+import vendor
+# insert `lib` as a site directory so our `main` module can load
+# third-party libraries, and override built-ins with newer
+# versions.
+vendor.add('lib')
