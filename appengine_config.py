5ac23a5e32a2a99cca6b4bbfaea4c24c4b76553b
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-03-25 17:02:21 -0700

    Removing vendor.py, vendoring is now part of the SDK.

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-import vendor
+from google.appengine.ext import vendor
+
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
